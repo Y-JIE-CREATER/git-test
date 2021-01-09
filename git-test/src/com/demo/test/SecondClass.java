89715e823d4fefa8806a74198a583c9a6ec86fcf
LYJ <YJ_L2019@163.com>
2021-01-09 10:36:41 +0800

    测试一个SecondClass

---
+++ git-test/src/com/demo/test/SecondClass.java
@@ -0,0 +1,17 @@
+package com.demo.test;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+    public void test(){
+        int t = 0;
+        t++;
+        System.out.println(t);
+    }
+}
