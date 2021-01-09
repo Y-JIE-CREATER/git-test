fb5e68713d49387eab692b96068b466929229903
LYJ <YJ_L2019@163.com>
2021-01-09 11:26:55 +0800

    删除某些类

--- git-test/src/com/demo/test/SecondClass.java
+++ git-test/src/com/demo/test/SecondClass.java
@@ -19,19 +19,7 @@ public class SecondClass {
         t++;
         System.out.println(t);
     }
-    public void test04(){
-        int t = 0;
-        t++;
-        System.out.println(t);
-    }
-    public void  test88(){
 
-    }
-    public void test99(){
-        int t = 0;
-        t++;
-        System.out.println(t);
-    }
 
 
 }
